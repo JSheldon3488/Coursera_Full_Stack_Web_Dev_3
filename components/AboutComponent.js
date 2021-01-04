1ea05e1a93b54a6f312bd74d405734560fda7c4f
Justin Sheldon <JSheldon3488@Gmail.com>
2021-01-03 20:18:09 -0600

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, Text, FlatList, View } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 
 function History() {
@@ -37,20 +38,42 @@ class About extends Component {
                     hideChevron={true}
                 />
             );
+        };
+
+        if (this.props.leaders.isLoading) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card title="Corporate Leadership">
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
         }
-        
-        return(
-            <ScrollView keyboardShouldPersistTaps='always'>
-                <History />
-                <Card title="Corporate Leadership">
-                    <FlatList 
-                        data={this.props.leaders.leaders}
-                        renderItem={leader}
-                        keyExtractor={item => item.id.toString()}
-                    />
-                </Card>
-            </ScrollView>
-        );
+        else if (this.props.leaders.errMess) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card title="Corporate Leadership">
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else {
+            return(
+                <ScrollView keyboardShouldPersistTaps='always'>
+                    <History />
+                    <Card title="Corporate Leadership">
+                        <FlatList 
+                            data={this.props.leaders.leaders}
+                            renderItem={leader}
+                            keyExtractor={item => item.id.toString()}
+                        />
+                    </Card>
+                </ScrollView>
+            );
+        }
     }
 }
 
